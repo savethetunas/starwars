762d68b64f38e53cc4e2d1411adf68968af8f5bd
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-02 21:02:48 -0700

    Hello

--- nebulon.py
+++ nebulon.py
@@ -1,7 +1,18 @@
+### Author: Adam Fehse
+### Course: CSc 110
+### Description: Create a TIE fighter that can receive input
+### from the user and adjust in width.
 
+
+### \n causes output to be advanced to the next line.
+### The int() function converts the value into an integer.
 width = int(input('Enter number:\n'))
-# its 9 spaces wide foe empty strings due to
-# the cockpit is 9 spaces wide at the smallest.
+
+
+### I concatenated |[ to a space then multiplied that by the width variable.
+### Then concatenated a string with 9 spaces + a single space.
+### Then multiplied that by width plus to ]|.
+### Had to use an extra \ because it acts as escape char.
 print('|[' + ' ' * width + '         ' + ' ' * width + ']|')
 print('|[' + ' ' * width + ' /=---=\\ ' + ' ' * width + ']|')
 print('|[' + '/' * width + '|== X ==|' + '\\' * width + ']|')
