c361075ceba934a6890f36714c4d9fff9aa3d088
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-04 13:28:55 -0700

    69

--- nebulon.py
+++ nebulon.py
@@ -7,10 +7,10 @@ front_minus_four = int(front - 4)
 
 front_divide_two = int(front / 2)
 front_divide_modulas_two = int(front / 2) + (front % 2)
-front_modulas_two = int(front % 2)
 
 front_divide_three = int(front / 3)
 
+hash_line_space = int((front * .7))
 
 print('  /='+ '-' * front +'\                 /--------|')
 print(' /=='+ '/' * front +'====\            |=========|')
@@ -19,7 +19,6 @@ print(' \\'+ '=' * front +'====-------------------------|')
 print('  \='+ '-' * front +'=///              \=======/')
 print((('    \\' + '-' * front_minus_four + '|') + '\n') * large, end='')
 print(((' ' * front_divide_modulas_two + ':' + '+' * front_divide_two + '|') +'\n') * medium, end='')
-print('       \\' + '#' * front_divide_three  + '|')
-
+print((' ' * hash_line_space + '\\' + '#' * front_divide_three + '|' + '\n') * small, end='')
 
 
