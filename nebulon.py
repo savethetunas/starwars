1517e4ea8389a262807226de58e25c42e86ab09c
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-03 21:27:05 -0700

    69

--- nebulon.py
+++ nebulon.py
@@ -1,17 +1,25 @@
+large = int(input('Large Layers on bottom:\n'))
+medium = int(input('Medium Layers on bottom:\n'))
+small = int(input('Small Layers on bottom:\n'))
+front = int(input('Front length:\n'))
+
+front_minus_four = int(front - 4)
+
+front_divide_two = int(front / 2)
+front_divide_modulas_two = int(front / 2) + (front % 2)
+front_modulas_two = int(front % 2)
+
+front_divide_three = int(front / 3)
+
+
+print('  /='+ '-' * front +'\                 /--------|')
+print(' /=='+ '/' * front +'====\            |=========|')
+print('|==-'+ '/' * front +'======\----================|')
+print(' \\'+ '=' * front +'====-------------------------|')
+print('  \='+ '-' * front +'=///              \=======/')
+print((('    \\' + '-' * front_minus_four + '|') + '\n') * large, end='')
+print(((' ' * front_divide_modulas_two + ':' + '+' * front_divide_two + '|') +'\n') * medium, end='')
+print('       \\' + '#' * front_divide_three  + '|')
 
 
 
-front = int(input('ok\n'))
-
-neck = int(input('Neck Length:\n'))
-body = int(input('Body Height:\n'))
-leg = int(input('Leg Height:\n'))
-
-print('  /=----------\                 /--------|')
-print(' /==//////////====\            |=========|')
-print('|==-//////////======\----================|')
-print(' \==============-------------------------|')
-print('  \=----------=///              \=======/')
-print('    \------|')
-print('     :+++++|')
-print('       \###|')
