c5fc42b40585f267460734b019ddf67b3ab7591c
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-03 21:25:43 -0700

    pp
    dh

--- nebulon.py
+++ nebulon.py
@@ -1,20 +1,17 @@
-### Author: Adam Fehse
-### Course: CSc 110
-### Description: Create a TIE fighter that can receive input
-### from the user and adjust in width.
 
 
-### \n causes output to be advanced to the next line.
-### The int() function converts the value into an integer.
-width = int(input('Enter number:\n'))
 
+front = int(input('ok\n'))
 
-### I concatenated |[ to a space then multiplied that by the width variable.
-### Then concatenated a string with 9 spaces + a single space.
-### Then multiplied that by width plus to ]|.
-### Had to use an extra \ because it acts as escape char.
-print('|[' + ' ' * width + '         ' + ' ' * width + ']|')
-print('|[' + ' ' * width + ' /=---=\\ ' + ' ' * width + ']|')
-print('|[' + '/' * width + '|== X ==|' + '\\' * width + ']|')
-print('|[' + ' ' * width + ' \\=---=/ ' + ' ' * width + ']|')
-print('|[' + ' ' * width + '         ' + ' ' * width + ']|')
+neck = int(input('Neck Length:\n'))
+body = int(input('Body Height:\n'))
+leg = int(input('Leg Height:\n'))
+
+print('  /=----------\                 /--------|')
+print(' /==//////////====\            |=========|')
+print('|==-//////////======\----================|')
+print(' \==============-------------------------|')
+print('  \=----------=///              \=======/')
+print('    \------|')
+print('     :+++++|')
+print('       \###|')
